c20add83ad71cb0de69372dc6dc9d483b6ee587e
Sushil Soni <sushilsoni281@gmail.com>
2024-09-10 12:49:24 +0530

    Update DB.js

--- DB.js
+++ DB.js
@@ -1,4 +1,4 @@
 let mongoose=require('mongoose');
 module.exports.init=async function(){
-    await mongoose.connect('mongodb+srv://sushilsoni281:no1cares@cluster0.d8d2gtv.mongodb.net/e-shop?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect('mongodb+srv://s****lsoni281:********@cluster0.d8d2gtv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 } 
